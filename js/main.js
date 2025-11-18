3d2a23b19f2c4dc4846ca0087187753fad94bb5d
Thierry <thierryabp17@gmail.com>
2025-11-17 21:44:25 -0300

    validacao de login feita

--- js/main.js
+++ js/main.js
@@ -2,12 +2,23 @@ let email = document.getElementById('email');
 let senha = document.getElementById('senha');
 let result = document.getElementById('result');
 
-function validarEmail() {
-    if (email.value != "" && email.value.length >= 10 && email.value.includes("@") && email.value.includes(".")) {
+function mostrarResultado() {
+    const valorEmail = email.value;
+
+    const validarEmail = (valorEmail) => {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(valorEmail);
+    };
+
+    let email_valido = validarEmail(valorEmail);
+
+    if (email_valido && valorEmail.length >= 10) {
         result.style.color = '#70e000';
-        result.innerHTML = "LOGIN EFETUADO";
-    }else {
-        result.style.color = 'red';
-        result.innerHTML = "LOGIN FRACASSADO";
+        result.style.marginTop = '0.5em';
+        result.innerHTML = "E-MAIL VÁLIDO!";
+    } else {
+        result.style.color = '#c1121f';
+        result.style.marginTop = '0.5em';
+        result.innerHTML = "E-MAIL INVÁLIDO!";
     }
 }
