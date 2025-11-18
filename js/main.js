ab72bc47df020d68e9392be4eb8ee3e31d131dd3
Thierry <thierryabp17@gmail.com>
2025-11-18 12:44:36 -0300

    validacao para senha

--- js/main.js
+++ js/main.js
@@ -1,24 +1,49 @@
 let email = document.getElementById('email');
 let senha = document.getElementById('senha');
-let result = document.getElementById('result');
+let success = document.getElementById('success');
+let error = document.getElementById('error_message');
 
 function mostrarResultado() {
     const valorEmail = email.value;
+    const valorSenha = senha.value;
 
     const validarEmail = (valorEmail) => {
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return regex.test(valorEmail);
-    };
+    }
+
+    const validarSenha = (valorSenha) => {
+        const regexSenha = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])(?:([0-9a-zA-Z$*&@#])(?!\1)){6,}$/;
+        return regexSenha.test(valorSenha);
+    }
 
     let email_valido = validarEmail(valorEmail);
+    let senha_valida = validarSenha(valorSenha);
 
-    if (email_valido && valorEmail.length >= 10) {
-        result.style.color = '#70e000';
-        result.style.marginTop = '0.5em';
-        result.innerHTML = "E-MAIL VÁLIDO!";
+    if ((email_valido && senha_valida) && (valorEmail.length >= 10)) {
+        success.style.marginTop = '0.4em';
+        success.innerHTML = "LOGIN EFETUADO!";
+        error.innerHTML = "";
+        error.style.marginTop = '0';
+    } else if (email_valido) {
+        error.style.marginTop = '0.3em';
+        error.innerHTML = "PREENCHA SUA SENHA!";
+        success.innerHTML = "";
+        success.style.marginTop = '0';
+    } else if (senha_valida) {
+        error.style.marginTop = '0.3em';
+        error.innerHTML = "PREENCHA SEU E-MAIL!";
+        success.innerHTML = "";
+        success.style.marginTop = '0';
+    } else if (senha_valida && valorSenha.length < 6) {
+        error.style.marginTop = '0.3em';
+        error.innerHTML = "SUA SENHA PRECISA TER AO MENOS 6 CARACTERES!";
+        success.innerHTML = "";
+        success.style.marginTop = '0';
     } else {
-        result.style.color = '#c1121f';
-        result.style.marginTop = '0.5em';
-        result.innerHTML = "E-MAIL INVÁLIDO!";
+        error.style.marginTop = '0.3em';
+        error.innerHTML = "E-MAIL INVÁLIDO!";
+        success.innerHTML = "";
+        success.style.marginTop = '0';
     }
 }
